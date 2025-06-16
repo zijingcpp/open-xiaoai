feaf0c09a5e3059f40a395ba0b0a6c33132b8fcf
Del Wang <wjg12341234@163.com>
2025-06-16 21:52:29 +0800

    chore: 更新配置文件 DEVICE_ID 默认值

--- examples/xiaozhi/config.py
+++ examples/xiaozhi/config.py
@@ -65,8 +65,8 @@ APP_CONFIG = {
     "xiaozhi": {
         "OTA_URL": "https://api.tenclass.net/xiaozhi/ota/",
         "WEBSOCKET_URL": "wss://api.tenclass.net/xiaozhi/v1/",
-        "WEBSOCKET_ACCESS_TOKEN": "（可选）一般用不到这个值",
-        "DEVICE_ID": "（可选）默认自动生成",
-        "VERIFICATION_CODE": "首次登陆时，验证码会在这里更新",
+        "WEBSOCKET_ACCESS_TOKEN": "", #（可选）一般用不到这个值
+        "DEVICE_ID": "", #（可选）默认自动生成
+        "VERIFICATION_CODE": "", # 首次登陆时，验证码会在这里更新
     },
 }
